8c3b5068a3a96eab429df91dae0b2c019c8d792c
Maciej Stefaniak <madragonse@gmail.com>
2024-02-23 21:54:09 +0100

    dev

--- mas2/create_input_datasets/helpers.py
+++ mas2/create_input_datasets/helpers.py
@@ -41,7 +41,7 @@ def load_all_datasets(datasets_path:str="datasets/") -> tuple:
     cbr_augmentation = pd.read_csv(f"{datasets_path}_cbr_augmentation.csv")
     cbr_database = pd.read_csv(f"{datasets_path}_cbr_database.csv")
     model_training = pd.read_csv(f"{datasets_path}_model_training.csv")
-    validation = pd.read_csv(f"{datasets_path}_validation_half.csv")
+    validation = pd.read_csv(f"{datasets_path}_validation.csv")
 
     return additional_cbr_1, additional_cbr_2, additional_validation, cbr_augmentation, cbr_database, model_training, validation
 
